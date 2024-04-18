81644aad06fd1d98c6b6b0e426099b1da81748d8
PatchRequest <47796889+PatchRequest@users.noreply.github.com>
2024-04-18 20:43:08 +0200

    pricing maybe

--- pages/index.vue
+++ pages/index.vue
@@ -69,153 +69,60 @@
     </div>
   </div>
 
-  <!--
-  <div id="subscribe" class="bg-gray-900 py-12 sm:py-16">
-    <div class="mx-auto max-w-7xl px-6 lg:px-8">
-      <div class="mx-auto mt-16 max-w-2xl rounded-3xl ring-1 ring-gray-200 sm:mt-20 lg:mx-0 lg:flex lg:max-w-none">
-        <div class="p-8 sm:p-10 lg:flex-auto">
-          <h3 class="text-2xl font-bold tracking-tight text-white">Subscribe to all data</h3>
-          <p class="mt-6 text-base leading-7 text-gray-300">We're currently offering only one subscription plan, which
-            will give you access to all found security issues in a websocket stream</p>
-          <div class="mt-10 flex items-center gap-x-4">
-            <h4 class="flex-none text-sm font-semibold leading-6 text-white">What’s included</h4>
-            <div class="h-px flex-auto bg-gray-100"/>
-          </div>
-          <ul role="list" class="mt-8 grid grid-cols-1 gap-4 text-sm leading-6 text-gray-600 sm:grid-cols-2 sm:gap-6">
-            <li v-for="feature in includedFeatures" :key="feature" class="flex gap-x-3 text-gray-400">
-              <CheckIcon class="h-6 w-5 flex-none text-rose-600" aria-hidden="true"/>
-              {{ feature }}
-            </li>
-          </ul>
-        </div>
-        <div class="-mt-2 p-2 lg:mt-0 lg:w-full lg:max-w-md lg:flex-shrink-0">
-          <div
-              class="rounded-2xl bg-white/5 py-10 text-center ring-1 ring-inset ring-gray-900/5 lg:flex lg:flex-col lg:justify-center lg:py-16">
-            <div class="mx-auto max-w-xs px-8">
-              <p class="text-base font-semibold text-gray-300">Pay once, own it forever</p>
-              <p class="mt-6 flex items-baseline justify-center gap-x-2">
-                <span class="text-5xl font-bold tracking-tight text-gray-200">50</span>
-                <span class="text-sm font-semibold leading-6 tracking-wide text-gray-300">EUR</span>
-              </p>
-              <a href="#"
-                 class="mt-10 block w-full rounded-md bg-rose-600 px-3 py-2 text-center text-sm font-semibold text-white shadow-sm hover:bg-rose-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-rose-600">
-                Get access
-              </a>
-              <p class="mt-6 text-xs leading-5 text-gray-300">Invoices and receipts available for easy company
-                reimbursement</p>
-            </div>
-          </div>
-        </div>
+
+  <div class="isolate overflow-hidden bg-gray-900">
+    <div class="mx-auto max-w-7xl px-6 pb-96 pt-24 text-center sm:pt-32 lg:px-8">
+      <div class="mx-auto max-w-4xl">
+        <h2 class="text-base font-semibold leading-7 text-indigo-400">Pricing</h2>
+        <p class="mt-2 text-4xl font-bold tracking-tight text-white sm:text-5xl">The right price for you, <br class="hidden sm:inline lg:hidden" />whoever you are</p>
+      </div>
+      <div class="relative mt-6">
+        <p class="mx-auto max-w-2xl text-lg leading-8 text-white/60">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Velit numquam eligendi quos odit doloribus molestiae voluptatum.</p>
+        <svg viewBox="0 0 1208 1024" class="absolute -top-10 left-1/2 -z-10 h-[64rem] -translate-x-1/2 [mask-image:radial-gradient(closest-side,white,transparent)] sm:-top-12 md:-top-20 lg:-top-12 xl:top-0">
+          <ellipse cx="604" cy="512" fill="url(#6d1bd035-0dd1-437e-93fa-59d316231eb0)" rx="604" ry="512" />
+          <defs>
+            <radialGradient id="6d1bd035-0dd1-437e-93fa-59d316231eb0">
+              <stop stop-color="#7775D6" />
+              <stop offset="1" stop-color="#E935C1" />
+            </radialGradient>
+          </defs>
+        </svg>
       </div>
     </div>
-  </div>
-  -->
-
-  <div class="isolate overflow-hidden">
-    <div class="flow-root pb-16 pt-24 sm:pt-32 lg:pb-0">
-      <div class="mx-auto max-w-7xl px-6 lg:px-8">
-        <div class="relative z-10">
-          <h2 class="mx-auto max-w-4xl text-center text-5xl font-bold tracking-tight text-white">Simple pricing, with free trial</h2>
-          <p class="mx-auto mt-4 max-w-2xl text-center text-lg leading-8 text-white/60">
-            Choose the plan that works for you. All plans include a 2-day free trial.
-            All plans can be changed or canceled at any time.
-          </p>
-          <div class="mt-16 flex justify-center">
-            <RadioGroup v-model="frequency" class="grid grid-cols-2 gap-x-1 rounded-full bg-white/5 p-1 text-center text-xs font-semibold leading-5 text-white">
-              <RadioGroupLabel class="sr-only">Payment frequency</RadioGroupLabel>
-              <RadioGroupOption as="template" v-for="option in frequencies" :key="option.value" :value="option" v-slot="{ checked }">
-                <div :class="[checked ? 'bg-rose-500' : '', 'cursor-pointer rounded-full px-2.5 py-1']">
-                  <span>{{ option.label }}</span>
+    <div class="flow-root bg-white pb-24 sm:pb-32">
+      <div class="-mt-80">
+        <div class="mx-auto max-w-7xl px-6 lg:px-8">
+          <div class="mx-auto grid max-w-md grid-cols-1 gap-8 lg:max-w-4xl lg:grid-cols-2">
+            <div v-for="tier in tiers" :key="tier.id" class="flex flex-col justify-between rounded-3xl bg-white p-8 shadow-xl ring-1 ring-gray-900/10 sm:p-10">
+              <div>
+                <h3 :id="tier.id" class="text-base font-semibold leading-7 text-indigo-600">{{ tier.name }}</h3>
+                <div class="mt-4 flex items-baseline gap-x-2">
+                  <span class="text-5xl font-bold tracking-tight text-gray-900">{{ tier.priceMonthly }}</span>
+                  <span class="text-base font-semibold leading-7 text-gray-600">/month</span>
                 </div>
-              </RadioGroupOption>
-            </RadioGroup>
-          </div>
-        </div>
-        <div class="relative mx-auto mt-10 grid max-w-md grid-cols-1 gap-y-8 lg:mx-0 lg:-mb-14 lg:max-w-none lg:grid-cols-3">
-          <svg viewBox="0 0 1208 1024" aria-hidden="true" class="absolute -bottom-48 left-1/2 h-[64rem] -translate-x-1/2 translate-y-1/2 [mask-image:radial-gradient(closest-side,white,transparent)] lg:-top-48 lg:bottom-auto lg:translate-y-0">
-            <ellipse cx="604" cy="512" fill="url(#d25c25d4-6d43-4bf9-b9ac-1842a30a4867)" rx="604" ry="512" />
-            <defs>
-              <radialGradient id="d25c25d4-6d43-4bf9-b9ac-1842a30a4867">
-                <stop stop-color="#db2777" />
-                <stop offset="1" stop-color="#831843" />
-              </radialGradient>
-            </defs>
-          </svg>
-          <div class="hidden lg:absolute lg:inset-x-px lg:bottom-0 lg:top-4 lg:block lg:rounded-t-2xl lg:bg-gray-800/80 lg:ring-1 lg:ring-white/10" aria-hidden="true" />
-          <div v-for="tier in tiers" :key="tier.id" :class="[tier.featured ? 'z-10 bg-white shadow-xl ring-1 ring-gray-900/10' : 'bg-gray-800/80 ring-1 ring-white/10 lg:bg-transparent lg:pb-14 lg:ring-0', 'relative rounded-2xl']">
-            <div class="p-8 lg:pt-12 xl:p-10 xl:pt-14">
-              <h3 :id="tier.id" :class="[tier.featured ? 'text-gray-900' : 'text-white', 'text-sm font-semibold leading-6']">{{ tier.name }}</h3>
-              <div class="flex flex-col gap-6 sm:flex-row sm:items-end sm:justify-between lg:flex-col lg:items-stretch">
-                <div class="mt-2 flex items-center gap-x-4">
-                  <p :class="[tier.featured ? 'text-gray-900' : 'text-white', 'text-4xl font-bold tracking-tight']">{{ tier.price[frequency.value] }}</p>
-                  <div class="text-sm leading-5">
-                    <p :class="tier.featured ? 'text-gray-900' : 'text-white'">USD</p>
-                    <p :class="tier.featured ? 'text-gray-500' : 'text-gray-400'">{{ `Billed ${frequency.value}` }}</p>
-                  </div>
-                </div>
-                <a :href="tier.href" :aria-describedby="tier.id" :class="[tier.featured ? 'bg-rose-600 shadow-sm hover:bg-rose-500 focus-visible:outline-rose-600' : 'bg-white/10 hover:bg-white/20 focus-visible:outline-white', 'rounded-md py-2 px-3 text-center text-sm font-semibold leading-6 text-white focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2']">
-                  Subscribe now
-                </a>
-              </div>
-              <div class="mt-8 flow-root sm:mt-10">
-                <ul role="list" :class="[tier.featured ? 'divide-gray-900/5 border-gray-900/5 text-gray-600' : 'divide-white/5 border-white/5 text-white', '-my-2 divide-y border-t text-sm leading-6 lg:border-t-0']">
-                  <li v-for="mainFeature in tier.mainFeatures" :key="mainFeature" class="flex gap-x-3 py-2">
-                    <CheckIcon :class="[tier.featured ? 'text-rose-600' : 'text-gray-500', 'h-6 w-5 flex-none']" aria-hidden="true" />
-                    {{ mainFeature }}
+                <p class="mt-6 text-base leading-7 text-gray-600">{{ tier.description }}</p>
+                <ul role="list" class="mt-10 space-y-4 text-sm leading-6 text-gray-600">
+                  <li v-for="feature in tier.features" :key="feature" class="flex gap-x-3">
+                    <CheckIcon class="h-6 w-5 flex-none text-indigo-600" aria-hidden="true" />
+                    {{ feature }}
                   </li>
                 </ul>
               </div>
+              <a :href="tier.href" :aria-describedby="tier.id" class="mt-8 block rounded-md bg-indigo-600 px-3.5 py-2 text-center text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Get started today</a>
+            </div>
+            <div class="flex flex-col items-start gap-x-8 gap-y-6 rounded-3xl p-8 ring-1 ring-gray-900/10 sm:gap-y-10 sm:p-10 lg:col-span-2 lg:flex-row lg:items-center">
+              <div class="lg:min-w-0 lg:flex-1">
+                <h3 class="text-lg font-semibold leading-8 tracking-tight text-indigo-600">Discounted</h3>
+                <p class="mt-1 text-base leading-7 text-gray-600">Dolor dolores repudiandae doloribus. Rerum sunt aut eum. Odit omnis non voluptatem sunt eos nostrum.</p>
+              </div>
+              <a href="#" class="rounded-md px-3.5 py-2 text-sm font-semibold leading-6 text-indigo-600 ring-1 ring-inset ring-indigo-200 hover:ring-indigo-300 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Buy discounted license <span aria-hidden="true">&rarr;</span></a>
             </div>
           </div>
         </div>
       </div>
     </div>
-
-    <div class="relative bg-gray-900">
-      <div class="mx-auto max-w-7xl px-6 lg:px-8">
-        <!-- Feature comparison (up to lg) -->
-        <section aria-labelledby="mobile-comparison-heading" class="lg:hidden">
-          <h2 id="mobile-comparison-heading" class="sr-only">Feature comparison</h2>
-
-          <div class="mx-auto max-w-2xl space-y-16">
-            <div v-for="tier in tiers" :key="tier.id" class="border-t border-gray-900/10">
-              <div :class="[tier.featured ? 'border-rose-600' : 'border-transparent', '-mt-px w-72 border-t-2 pt-10 md:w-80']">
-                <h3 :class="[tier.featured ? 'text-rose-600' : 'text-gray-900', 'text-sm font-semibold leading-6']">{{ tier.name }}</h3>
-                <p class="mt-1 text-sm leading-6 text-gray-600">{{ tier.description }}</p>
-              </div>
-
-              <div class="mt-10 space-y-10">
-                <div v-for="section in sections" :key="section.name">
-                  <h4 class="text-sm font-semibold leading-6 text-gray-900">{{ section.name }}</h4>
-                  <div class="relative mt-6">
-                    <div aria-hidden="true" class="absolute inset-y-0 right-0 hidden w-1/2 rounded-lg bg-white shadow-sm sm:block" />
-
-                    <div :class="[tier.featured ? 'ring-2 ring-rose-600' : 'ring-1 ring-gray-900/10', 'relative rounded-lg bg-white shadow-sm sm:rounded-none sm:bg-transparent sm:shadow-none sm:ring-0']">
-                      <dl class="divide-y divide-gray-200 text-sm leading-6">
-                        <div v-for="feature in section.features" :key="feature.name" class="flex items-center justify-between px-4 py-3 sm:grid sm:grid-cols-2 sm:px-0">
-                          <dt class="pr-4 text-gray-600">{{ feature.name }}</dt>
-                          <dd class="flex items-center justify-end sm:justify-center sm:px-4">
-                            <span v-if="typeof feature.tiers[tier.name] === 'string'" :class="tier.featured ? 'font-semibold text-rose-600' : 'text-gray-900'">{{ feature.tiers[tier.name] }}</span>
-                            <template v-else>
-                              <CheckIcon v-if="feature.tiers[tier.name] === true" class="mx-auto h-5 w-5 text-rose-600" aria-hidden="true" />
-                              <XMarkIcon v-else class="mx-auto h-5 w-5 text-gray-400" aria-hidden="true" />
-                              <span class="sr-only">{{ feature.tiers[tier.name] === true ? 'Yes' : 'No' }}</span>
-                            </template>
-                          </dd>
-                        </div>
-                      </dl>
-                    </div>
-
-                    <div aria-hidden="true" :class="[tier.featured ? 'ring-2 ring-rose-600' : 'ring-1 ring-gray-900/10', 'pointer-events-none absolute inset-y-0 right-0 hidden w-1/2 rounded-lg sm:block']" />
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </section>
-      </div>
-    </div>
   </div>
+  
 
   <section id="testimonials" class="bg-gray-900 py-12 sm:py-16">
     <div class="mx-auto max-w-7xl px-6 lg:px-8">
@@ -325,55 +232,33 @@ import {Disclosure, DisclosureButton, DisclosurePanel} from '@headlessui/vue'
 import { ref } from 'vue'
 import { RadioGroup, RadioGroupLabel, RadioGroupOption } from '@headlessui/vue'
 import { CheckIcon, XMarkIcon } from '@heroicons/vue/20/solid'
+import { CheckIcon } from '@heroicons/vue/20/solid'
 
-const frequencies = [
-  { value: 'monthly', label: 'Monthly' }
-]
 const tiers = [
   {
-    name: 'Starter',
-    id: 'tier-starter',
+    name: 'Hobby',
+    id: 'tier-hobby',
     href: '#',
-    featured: false,
-    price: { monthly: '$15' },
-    mainFeatures: [
-        'Access to 3 issue categories',
-        'No rate limits',
-        'Get 75% of the hits',
-        'Only billing support via e-mail'
-    ],
+    priceMonthly: '$49',
+    description: 'Modi dolorem expedita deleniti. Corporis iste qui inventore pariatur adipisci vitae.',
+    features: ['5 products', 'Up to 1,000 subscribers', 'Basic analytics', '48-hour support response time'],
   },
   {
-    name: 'Scale',
-    id: 'tier-scale',
+    name: 'Team',
+    id: 'tier-team',
     href: '#',
-    featured: true,
-
-    price: { monthly: '$60', annually: '$576' },
-    mainFeatures: [
-        'Access to all issue categories',
-        'Access to our monitoring tool',
-        'Get 100% of the hits',
-        'Full Support via e-mail or chat'
-    ],
-  },
-  {
-    name: 'Growth',
-    id: 'tier-growth',
-    href: '#',
-    featured: false,
-    price: { monthly: '$30', annually: '$288' },
-    mainFeatures: [
-        'Access to 6 issue categories',
-        'Access to our monitoring tool',
-        'Get 85% of the hits',
-        'Billing support via e-mail'
+    priceMonthly: '$79',
+    description: 'Explicabo quo fugit vel facere ullam corrupti non dolores. Expedita eius sit sequi.',
+    features: [
+      'Unlimited products',
+      'Unlimited subscribers',
+      'Advanced analytics',
+      '1-hour, dedicated support response time',
+      'Marketing automations',
     ],
   },
 ]
 
-
-const frequency = ref(frequencies[0])
 
 useHead({
   title: 'Home | Cerast Intelligence',
@@ -407,12 +292,7 @@ const faqs = [
   },
 ]
 
-const includedFeatures = [
-  'E-Mail Support',
-  'Websocket stream',
-  'Access to all security issues',
-  'No rate limits',
-]
+
 
 const stats = [
   {id: 1, name: 'Findings last month', value: '86,400+'},
