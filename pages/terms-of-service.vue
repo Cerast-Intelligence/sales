35dbb1e13a564f0ddaf2f8d60002c7d9b2e52436
PatchRequest <47796889+PatchRequest@users.noreply.github.com>
2024-04-18 12:53:49 +0200

    Update terms-of-service.vue

--- pages/terms-of-service.vue
+++ pages/terms-of-service.vue
@@ -3,7 +3,7 @@
 </script>
 
 <template>
-  <main class="relative isolate text-white">
+  <main class="relative isolate text-white flex">
   <h1>Terms of Service for DataStream Subscrip text-whitetion</h1>
 
     <h2>1. Acceptance of Terms</h2>
