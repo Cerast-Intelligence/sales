6a487fc9d325afc23d981e800da676ae71928b26
PatchRequest <47796889+PatchRequest@users.noreply.github.com>
2024-04-18 12:46:35 +0200

    Update terms-of-service.vue

--- pages/terms-of-service.vue
+++ pages/terms-of-service.vue
@@ -3,7 +3,52 @@
 </script>
 
 <template>
+Terms of Service for DataStream Subscription
 
+1. Acceptance of Terms
+By subscribing to and using the DataStream service ("Service"), you agree to be bound by these Terms of Service ("Terms"). If you do not agree to these Terms, do not use the Service.
+
+2. Service Description
+The Service provides a subscription-based access to a data stream that you may use for research and analysis purposes. The Service does not guarantee uptime or the volume of data transmitted.
+
+3. Use of Data
+a. You may use the data provided through the Service for conducting research and for creating derivative works based on such research.
+b. You are expressly prohibited from publishing, reselling, or distributing the data received through the Service in any form.
+c. The data may not be used for any illegal activities or to support any illegal activities.
+
+4. User Responsibilities
+a. You must keep your access credentials confidential and not share them with any third party.
+b. You are responsible for all activities that occur under your account.
+c. You agree to immediately notify the Service provider of any unauthorized use of your account or any other breach of security.
+
+5. Prohibited Conduct
+Using the Service for the following is prohibited:
+a. Engaging in illegal activities or supporting illegal activities through the use of the data.
+b. Publishing, selling, or distributing the data outside the scope permitted by these Terms.
+
+6. Termination
+a. Any violation of these Terms will result in immediate termination of your subscription without any refund for the last billing period.
+b. The Service provider may terminate your access to the Service at any time, with or without cause, with or without notice, effective immediately.
+
+7. Disclaimer of Warranties
+The Service is provided on an "as is" and "as available" basis. The Service provider expressly disclaims all warranties of any kind, whether express or implied, including, but not limited to the implied warranties of merchantability, fitness for a particular purpose and non-infringement.
+
+8. Limitation of Liability
+In no event shall the Service provider be liable for any direct, indirect, incidental, special, consequential, or exemplary damages, including but not limited to, damages for loss of profits, goodwill, use, data or other intangible losses (even if the Service provider has been advised of the possibility of such damages), resulting from the use or the inability to use the Service.
+
+9. Indemnification
+You agree to indemnify and hold the Service provider, and its subsidiaries, affiliates, officers, agents, co-branders or other partners, and employees, harmless from any claim or demand, including reasonable attorneys' fees, made by any third party due to or arising out of your use of the Service, your connection to the Service, your violation of the Terms, or your violation of any rights of another.
+
+10. General Information
+These Terms constitute the entire agreement between you and the Service provider and govern your use of the Service, superseding any prior agreements between you and the Service provider. You also may be subject to additional terms and conditions that may apply when you use affiliate services, third-party content, or third-party software.
+
+11. Changes to Terms
+The Service provider reserves the right to update and change, from time to time, these Terms and all documents incorporated by reference. You can always find the most recent version of these Terms at [insert URL here].
+
+12. Contact Information
+If you have any questions about these Terms or the Service, please contact us at [insert contact information here].
+
+By subscribing to and using the Service, you acknowledge that you have read, understood, and agree to be bound by these Terms.
 </template>
 
 <style scoped>
