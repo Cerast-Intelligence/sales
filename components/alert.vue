8613971fec1a5219a1d59d4edfe4c1fb83a44f0b
Marvin Vomberg <mvomberg@byte5.de>
2024-05-04 21:01:28 +0200

    fix ico and color of success

--- components/alert.vue
+++ components/alert.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-      :class="{ 'bg-blue-600': type === 'info', 'bg-rose-600': type === 'error', 'bg-yellow-600': type === 'warning', 'bg-green-500': type === 'success' }"
+      :class="{ 'bg-blue-600': type === 'info', 'bg-rose-600': type === 'error', 'bg-yellow-600': type === 'warning', 'emerald-600': type === 'success' }"
       class="rounded-md p-4 text-white">
     <div class="flex">
       <div class="flex-shrink-0">
@@ -12,7 +12,7 @@
       <div class="ml-auto pl-3">
         <div class="-mx-1.5 -my-1.5">
           <button @click="$emit('close')" type="button"
-                  :class="{'bg-green-50 text-green-500 hover:bg-green-100 focus:ring-green-600 focus:ring-offset-green-50': type === 'success',
+                  :class="{'bg-emerald-50 text-emerald-500 hover:bg-emerald-100 focus:ring-emerald-600 focus:ring-offset-emerald-50': type === 'success',
                            'bg-yellow-50 text-yellow-500 hover:bg-yellow-100 focus:ring-yellow-600 focus:ring-offset-yellow-50': type === 'warning',
                            'bg-rose-50 text-rose-500 hover:bg-rose-100 focus:ring-rose-600 focus:ring-offset-rose-50': type === 'error',
                            'bg-blue-50 text-blue-500 hover:bg-blue-100 focus:ring-blue-600 focus:ring-offset-blue-50': type === 'info'}"
