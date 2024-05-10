831ab0b0cd0cd6c0f95c14c473e9498612a736e2
Marvin Vomberg <mvomberg@byte5.de>
2024-05-10 22:53:08 +0200

    remove pattern not visible

--- pages/index.vue
+++ pages/index.vue
@@ -150,21 +150,6 @@
 
 
   <div id="subscribe" class="bg-gray-900 py-16 sm:py-24 relative">
-    <svg
-        class="absolute inset-0 -z-10 h-full w-full stroke-white/10 [mask-image:radial-gradient(100%_100%_at_top_right,white,transparent)]"
-        aria-hidden="true">
-      <defs>
-        <pattern id="983e3e4c-de6d-4c3f-8d64-b9761d1534cc" width="200" height="200" x="50%" y="-1"
-                 patternUnits="userSpaceOnUse">
-          <path d="M.5 200V.5H200" fill="none"/>
-        </pattern>
-      </defs>
-      <svg x="50%" y="-1" class="overflow-visible fill-gray-800/20">
-        <path d="M-200 0h201v201h-201Z M600 0h201v201h-201Z M-400 600h201v201h-201Z M200 800h201v201h-201Z"
-              stroke-width="0"/>
-      </svg>
-      <rect width="100%" height="100%" stroke-width="0" fill="url(#983e3e4c-de6d-4c3f-8d64-b9761d1534cc)"/>
-    </svg>
     <div class="mx-auto max-w-7xl px-6 lg:px-8">
       <div class="mx-auto max-w-4xl text-center">
         <h2 class="text-base font-semibold leading-7 text-rose-600">Pricing</h2>
