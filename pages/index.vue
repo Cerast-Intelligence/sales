d8c89547be9386122697834dfce7c5b55cd7b217
Marvin Vomberg <mvomberg@byte5.de>
2024-05-10 22:36:58 +0200

    fix anchor on pricing

--- pages/index.vue
+++ pages/index.vue
@@ -149,7 +149,7 @@
   </div>
 
 
-  <div class="bg-gray-900 py-16 sm:py-24">
+  <div id="subscribe" class="bg-gray-900 py-16 sm:py-24">
     <div class="mx-auto max-w-7xl px-6 lg:px-8">
       <div class="mx-auto max-w-4xl text-center">
         <h2 class="text-base font-semibold leading-7 text-rose-600">Pricing</h2>
