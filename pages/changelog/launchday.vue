36dc4d57158dae97a89d524c9b690fe709edf7e9
PatchRequest <me@danielriebel.com>
2024-05-12 17:54:06 +0200

    lorem ipsum lol

--- pages/changelog/launchday.vue
+++ pages/changelog/launchday.vue
@@ -22,7 +22,7 @@ const pages = [
     </div>
     <div class="mx-auto max-w-2xl text-center">
       <h2 class="text-4xl font-bold tracking-tight text-white sm:text-6xl">Launchday</h2>
-      <p class="mt-6 text-lg leading-8 text-gray-300">Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat fugiat aliqua.</p>
+      
     </div>
   </div>
 
