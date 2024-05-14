7e98a0b5024905800dc57a8aded66ffa6350fa26
PatchRequest <me@danielriebel.com>
2024-05-14 22:13:29 +0200

    imprint and text

--- pages/imprint.vue
+++ pages/imprint.vue
@@ -5,9 +5,10 @@
       <h1 class="mt-2 text-3xl font-bold tracking-tight text-white sm:text-4xl">Imprint</h1>
       <div class="mt-10 max-w-2xl">
         <h2 class="mt-16 text-2xl font-bold tracking-tight text-white">Company Information</h2>
-        <p><strong>Name:</strong> Cerast Intelligence</p>
+        <p><strong>Name:</strong> Cerast Intelligence Einzelunternehmen</p>
         <p><strong>Represented by:</strong> Daniel Riebel</p>
         <p><strong>Address:</strong> Dr. Dietz Straße 23, 36043 Fulda, Germany</p>
+        <p><strong>Steuernummer:</strong> 018 860 63236</p>
 
         <h2 class="mt-16 text-2xl font-bold tracking-tight text-white">Contact Information</h2>
         <p>Email: contact@cerast-intelligence.com</p>
